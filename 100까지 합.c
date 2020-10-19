5401d3c6eb0fe7f3979d7de3b9b76721f9639791
lliu4321 <73099236+lliu4321@users.noreply.github.com>
2020-10-19 16:43:26 +0900

    Add files via upload

---
+++ 100까지 합.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int i = 0;
+   int sum = 0;
+   while(i<=100){
+       sum = sum + i;
+       i = i + 2;
+   }
+   printf("0부터 100까지의 짝수의 합은?:%d\n",sum);
+    return 0;
+}
